33b6e8f073e7da8220b7953a4ff42c8eb115af57
Alexis <amaryllisno@gmail.com>
2021-12-02 11:17:55 +0100

    feat: navigation links to nav

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,9 +10,52 @@ const StyledSidebar = styled.aside`
   position: absolute;
   bottom: 0;
   background-color: ${({ theme }) => theme.colors.onBackground};
+  text-transform: uppercase;
+  font-size: 0.8rem;
+
+  font-family: 'atlas grotesk', sans-serif;
 
   @media only screen and (max-width: 768px) {
     display: none;
+  }
+`;
+
+const StyledNav = styled.nav`
+  display: flex;
+  padding: 0 2rem;
+  font-weight: normal;
+
+  ul {
+    list-style: none;
+  }
+`;
+
+const MainPageListItem = styled.li``;
+
+const MainPageLink = styled.div`
+  display: block;
+  font-size: 1rem;
+  font-weight: 500;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+`;
+
+const PortfolioList = styled.ul`
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+`;
+
+const ListItem = styled.li`
+  margin: 20px 10px 10px 10px;
+
+  a {
+    display: flex;
+    align-items: center;
+    vertical-align: middle;
+
+    img {
+      margin-right: 10px;
+    }
   }
 `;
 
@@ -22,9 +65,62 @@ const Sidebar = () => {
       <StyledSidebar>
         <Link href='/'>
           <a>
-            <Logo src='images/svg/amaryllis-text-logo-2022.svg'></Logo>
+            <Logo src='/images/svg/amaryllis-text-logo-2022.svg'></Logo>
           </a>
         </Link>
+        <StyledNav>
+          <ul>
+            <MainPageListItem>
+              <Link href='/'>
+                <MainPageLink>PORTFOLIO</MainPageLink>
+              </Link>
+              <PortfolioList>
+                <ListItem>
+                  <Link href='/dev'>
+                    <a>
+                      <img src='/images/svg/dev.svg' alt='' />
+                      <span>Web Dev.</span>
+                    </a>
+                  </Link>
+                </ListItem>
+                <ListItem>
+                  <Link href='/design'>
+                    <a>
+                      <img src='/images/svg/design.svg' alt='' />
+                      <span>Design</span>
+                    </a>
+                  </Link>
+                </ListItem>
+                <ListItem>
+                  <Link href='/art'>
+                    <a>
+                      <img src='/images/svg/art.svg' alt='' />
+                      <span>Art</span>
+                    </a>
+                  </Link>
+                </ListItem>
+                <ListItem>
+                  <Link href='/music'>
+                    <a>
+                      <img src='/images/svg/music.svg' alt='' />
+                      <span>Music</span>
+                    </a>
+                  </Link>
+                </ListItem>
+              </PortfolioList>
+            </MainPageListItem>
+            <MainPageListItem>
+              <Link href='/about'>
+                <MainPageLink>ABOUT ME</MainPageLink>
+              </Link>
+            </MainPageListItem>
+            <MainPageListItem>
+              <Link href='/contact'>
+                <MainPageLink>CONTACT</MainPageLink>
+              </Link>
+            </MainPageListItem>
+          </ul>
+        </StyledNav>
       </StyledSidebar>
     </div>
   );
